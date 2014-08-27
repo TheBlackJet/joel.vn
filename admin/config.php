43b2b0a9abe1fe8a86525089405b0997595bc129
John Le <nguyenlt_dev@yahoo.com>
2014-08-27 13:07:00 +1000

    this is the initial config set up

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8089/joel.vn/admin/');
+define('HTTP_CATALOG', 'http://localhost:8089/joel.vn/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8089/joel.vn/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8089/joel.vn/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\joel.vn/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\joel.vn/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\joel.vn/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\joel.vn/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\joel.vn/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\joel.vn/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\joel.vn/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\joel.vn/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\joel.vn/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\joel.vn/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\joel.vn/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'joelvn');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8089/joel.vn/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8089/joel.vn/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\joel.vn/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\joel.vn/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\joel.vn/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\joel.vn/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\joel.vn/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\joel.vn/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\joel.vn/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\joel.vn/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\joel.vn/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\joel.vn/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'joelvn');
+define('DB_PREFIX', 'oc_');
+?>
